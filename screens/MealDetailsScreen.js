6cf5d59e347e747d6961b9cb50091a31fa557368
cherucole <cheruyotcheruyot@gmail.com>
2019-12-02 14:41:02 +0300

    passing data to meals page

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
 
 const MealDetailsScreen = props => {
+  const mealId = props.navigation.getParam("mealId");
+
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
   return (
     <View style={styles.screen}>
-      <Text>The Meal Details Screen Screen</Text>
+      <Text>{selectedMeal.title}</Text>
     </View>
   );
+};
+
+MealDetailsScreen.navigationOptions = navigationData => {
+  const mealId = navigationData.navigation.getParam("mealId");
+
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal.title
+  };
 };
 
 const styles = StyleSheet.create({
