0b3e4046cb0a2dec9961b3b6f7d95e3f625ab856
cherucole <cheruyotcheruyot@gmail.com>
2019-12-02 11:59:04 +0300

    added navigation styling using dynamic passing

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,16 +1,32 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
+import { Platform } from "react-native";
+import Colors from "../constants/Colors";
+
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CatergoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailsScreen from "../screens/MealDetailsScreen";
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: {
-    screen: CatergoryMealsScreen
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen
+    },
+    CategoryMeals: {
+      screen: CatergoryMealsScreen
+    },
+    MealDetail: MealDetailsScreen
   },
-  MealDetail: MealDetailsScreen
-});
+  {
+    // mode: "modal",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS == "android" ? Colors.primaryColor : ""
+      },
+      headerTintColor: Platform.OS == "android" ? "white" : Colors.primaryColor
+    }
+  }
+);
 
 export default createAppContainer(MealsNavigator);
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Button, Platform } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 import { CATEGORIES } from "../data/dummy-data";
 import Colors from "../constants/Colors";
@@ -24,11 +24,7 @@ CategoryMealsScreen.navigationOptions = navigationData => {
   const catId = navigationData.navigation.getParam("categoryId");
   const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
   return {
-    headerTitle: selectedCategory.title,
-    headerStyle: {
-      backgroundColor: Platform.OS == "android" ? Colors.primaryColor : ""
-    },
-    headerTintColor: Platform.OS == "android" ? "white" : Colors.primaryColor
+    headerTitle: selectedCategory.title
   };
 };
 
